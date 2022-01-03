8c505db9c40f4dec2369a82191f6cc60ddb1465c
Danny Cairns <dpcairns@gmail.com>
2022-01-03 15:47:25 -0800

    Update fetch-utils.js

--- fetch-utils.js
+++ fetch-utils.js
@@ -4,12 +4,8 @@ const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsI
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export async function createCharacter(character){
-    const newCharacter = {
-        ...character, 
-        user_id: client.auth.user().id, 
-    };
 
-    // use the newCharacter to create a single new character for this user in supabase
+    // use the character to create a single new character for this user in supabase
     return checkError(response);
 }
 
